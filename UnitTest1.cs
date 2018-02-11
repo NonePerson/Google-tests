8d2ebb2d5a6bca8834e722853dda1be1f148fd1a
Secret <debategames2@gmail.com>
2018-02-11 20:27:10 +0200

    Initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class GoogleSiteTests
+    {
+        IWebDriver driver = new ChromeDriver();
+        [Test]
+        public void GoToSite()
+        {
+            driver.Navigate().GoToUrl("https://www.google.co.il");
+        }
+        public void TheRest()
+        {
+            // search input
+            By searchInput = By.Id("lst-ib");
+            IWebElement searchInputElement = driver.FindElement(searchInput);
+
+            IWebElement searchButton;
+            By TheInputElements = By.TagName("input");
+            IList<IWebElement> inputElements = driver.FindElements(TheInputElements);
+            foreach(IWebElement inputElement in inputElements)
+            {
+                if (inputElement.Text == "חיפוש ב-Google")
+                {
+                    searchButton = inputElement;
+                }
+            }
+        }
+       
+    }
+}
