6e6f053936bb3e49fcfc01653df2d4e3e4425675
Zahav Zilberman <26978002+ZahavZilberman@users.noreply.github.com>
2018-12-14 15:47:56 +0200

    Update UnitTest1.cs
    Fixed:
    - tearing down the driver
    - spiliting the steps to other functions
    - removing useless code

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,36 +1,64 @@
-﻿using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
 using System.Collections.Generic;
 
 namespace UnitTestProject1
 {
-    [TestFixture]
+    [TestClass]
     public class GoogleSiteTests
     {
         IWebDriver driver = new ChromeDriver();
+
+        public GoogleSiteTests()
+        {
+        }
+
+        [TestMethod]
+        public void GoogleHomePageTest()
+        {
+            GoToSite();
+
+            try
+            {
+                Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(IsTheGoogleSearchTextShown());
+            }
+            catch (AssertFailedException)
+            {
+                Close();
+                Microsoft.VisualStudio.TestTools.UnitTesting.Assert.Fail();
+            }
+        }
+
         [Test]
         public void GoToSite()
         {
             driver.Navigate().GoToUrl("https://www.google.co.il");
         }
-        public void TheRest()
-        {
-            // search input
-            By searchInput = By.Id("lst-ib");
-            IWebElement searchInputElement = driver.FindElement(searchInput);
 
-            IWebElement searchButton;
+        [Test]
+        public bool IsTheGoogleSearchTextShown()
+        {
+            IWebElement searchButton = null;
             By TheInputElements = By.TagName("input");
             IList<IWebElement> inputElements = driver.FindElements(TheInputElements);
-            foreach(IWebElement inputElement in inputElements)
+            foreach (IWebElement inputElement in inputElements)
             {
                 if (inputElement.Text == "חיפוש ב-Google")
                 {
                     searchButton = inputElement;
                 }
             }
+
+            return searchButton != null;
         }
-       
+
+        [TearDown]
+        public void Close()
+        {
+            this.driver.Close();
+        }
     }
 }
+
